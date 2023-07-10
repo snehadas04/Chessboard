b6e0084c0391f76a178454590862377e042d960e
Sneha Das <89447948+snehadas04@users.noreply.github.com>
2023-07-10 20:51:23 +0530

    Add files via upload

--- chessBoard.py
+++ chessBoard.py
@@ -7,3 +7,11 @@ def draw():
         cb.forward(30)
         cb.left(90)
     cb.forward(30)
+
+if __name__ == "__main__" :
+    sc.setup(400, 600)
+    cb.speed(100)
+    for i in range(8):
+        cb.up()
+        cb.setpos(-100,30*i)
+        cb.down()
